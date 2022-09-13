976f34d61cc96120df725ef8835e4dcbd8d204f5
PixelRGB <113426146+RGBPixel1999@users.noreply.github.com>
2022-09-13 10:47:08 +0200

    Add files via upload

---
+++ fxProject1/src/application/fx1SampleController.java
@@ -0,0 +1,5 @@
+package application;
+
+public class fx1SampleController {
+	
+}
---
+++ fxProject1/src/module-info.java
@@ -0,0 +1,6 @@
+module fxProject1 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
